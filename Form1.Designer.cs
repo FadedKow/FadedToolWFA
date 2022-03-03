25df1d0a35b859d3d4a9702dcf0ac3402013ebee
Founder <55168055+FadedKow@users.noreply.github.com>
2022-03-02 20:13:24 -0500

    1.18.10 (x64), 1.18.12 (x64), and x86 for 1.18.2
    update woo

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,6 +35,7 @@
             this.creatorLabel = new System.Windows.Forms.Label();
             this.whatThisDoesLabel = new System.Windows.Forms.Label();
             this.whatIsThisLabel = new System.Windows.Forms.Label();
+            this.cpuSwitch = new System.Windows.Forms.CheckBox();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -43,20 +44,22 @@
             this.tableLayoutPanel1.ColumnCount = 3;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 66.41679F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.58321F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 399F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 301F));
             this.tableLayoutPanel1.Controls.Add(this.versionSelectList, 1, 0);
             this.tableLayoutPanel1.Controls.Add(this.spoofButton, 1, 2);
             this.tableLayoutPanel1.Controls.Add(this.creatorLabel, 0, 2);
             this.tableLayoutPanel1.Controls.Add(this.whatThisDoesLabel, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.whatIsThisLabel, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.cpuSwitch, 2, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 3;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 47.88733F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 52.11267F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 127F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(1067, 554);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 103F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // versionSelectList
@@ -65,23 +68,25 @@
             this.versionSelectList.Dock = System.Windows.Forms.DockStyle.Fill;
             this.versionSelectList.ForeColor = System.Drawing.Color.Silver;
             this.versionSelectList.FormattingEnabled = true;
-            this.versionSelectList.ItemHeight = 16;
             this.versionSelectList.Items.AddRange(new object[] {
-            "1.18.2",
-            "1.18.0",
-            "1.17.41",
-            "1.17.40",
-            "1.17.10/1.17.11",
-            "1.17.2",
-            "1.17.0",
-            "1.16.221",
-            "1.16.210",
-            "1.16.201",
+            "1.16.40",
             "1.16.100",
-            "1.16.40"});
-            this.versionSelectList.Location = new System.Drawing.Point(446, 3);
+            "1.16.201",
+            "1.16.210",
+            "1.16.221",
+            "1.17.0",
+            "1.17.2",
+            "1.17.10/1.17.11",
+            "1.17.40",
+            "1.17.41",
+            "1.18.0",
+            "1.18.2",
+            "1.18.10.4",
+            "1.18.12.1"});
+            this.versionSelectList.Location = new System.Drawing.Point(333, 2);
+            this.versionSelectList.Margin = new System.Windows.Forms.Padding(2);
             this.versionSelectList.Name = "versionSelectList";
-            this.versionSelectList.Size = new System.Drawing.Size(218, 198);
+            this.versionSelectList.Size = new System.Drawing.Size(163, 162);
             this.versionSelectList.TabIndex = 0;
             this.versionSelectList.SelectedIndexChanged += new System.EventHandler(this.versionSelectList_SelectedIndexChanged);
             // 
@@ -90,9 +95,10 @@
             this.spoofButton.BackColor = System.Drawing.Color.DimGray;
             this.spoofButton.Dock = System.Windows.Forms.DockStyle.Top;
             this.spoofButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.spoofButton.Location = new System.Drawing.Point(446, 429);
+            this.spoofButton.Location = new System.Drawing.Point(333, 348);
+            this.spoofButton.Margin = new System.Windows.Forms.Padding(2);
             this.spoofButton.Name = "spoofButton";
-            this.spoofButton.Size = new System.Drawing.Size(218, 53);
+            this.spoofButton.Size = new System.Drawing.Size(163, 43);
             this.spoofButton.TabIndex = 1;
             this.spoofButton.Text = "Spoof";
             this.spoofButton.UseVisualStyleBackColor = false;
@@ -102,9 +108,10 @@
             // 
             this.creatorLabel.AutoSize = true;
             this.creatorLabel.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.creatorLabel.Location = new System.Drawing.Point(3, 538);
+            this.creatorLabel.Location = new System.Drawing.Point(2, 437);
+            this.creatorLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.creatorLabel.Name = "creatorLabel";
-            this.creatorLabel.Size = new System.Drawing.Size(437, 16);
+            this.creatorLabel.Size = new System.Drawing.Size(327, 13);
             this.creatorLabel.TabIndex = 2;
             this.creatorLabel.Text = "Created by Founder#8300";
             // 
@@ -112,36 +119,46 @@
             // 
             this.whatThisDoesLabel.AutoSize = true;
             this.whatThisDoesLabel.Dock = System.Windows.Forms.DockStyle.Top;
-            this.whatThisDoesLabel.Location = new System.Drawing.Point(3, 0);
+            this.whatThisDoesLabel.Location = new System.Drawing.Point(2, 0);
+            this.whatThisDoesLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.whatThisDoesLabel.Name = "whatThisDoesLabel";
-            this.whatThisDoesLabel.Size = new System.Drawing.Size(437, 96);
+            this.whatThisDoesLabel.Size = new System.Drawing.Size(327, 91);
             this.whatThisDoesLabel.TabIndex = 3;
-            this.whatThisDoesLabel.Text = "FadedTool - Simple Gui Edition\r\n\r\nHow To Use:\r\nStep 1: Select Your Minecraft Vers" +
-    "ion In The List.\r\nStep 2: Press Spoof\r\nThat\'s all, you can close the app now or " +
-    "keep it open for later.";
+            this.whatThisDoesLabel.Text = resources.GetString("whatThisDoesLabel.Text");
             // 
             // whatIsThisLabel
             // 
             this.whatIsThisLabel.AutoSize = true;
             this.tableLayoutPanel1.SetColumnSpan(this.whatIsThisLabel, 3);
             this.whatIsThisLabel.Dock = System.Windows.Forms.DockStyle.Top;
-            this.whatIsThisLabel.Location = new System.Drawing.Point(3, 204);
+            this.whatIsThisLabel.Location = new System.Drawing.Point(2, 166);
+            this.whatIsThisLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.whatIsThisLabel.Name = "whatIsThisLabel";
-            this.whatIsThisLabel.Size = new System.Drawing.Size(1061, 176);
+            this.whatIsThisLabel.Size = new System.Drawing.Size(796, 143);
             this.whatIsThisLabel.TabIndex = 4;
             this.whatIsThisLabel.Text = resources.GetString("whatIsThisLabel.Text");
             // 
+            // cpuSwitch
+            // 
+            this.cpuSwitch.AutoSize = true;
+            this.cpuSwitch.Dock = System.Windows.Forms.DockStyle.Top;
+            this.cpuSwitch.Location = new System.Drawing.Point(501, 3);
+            this.cpuSwitch.Name = "cpuSwitch";
+            this.cpuSwitch.Size = new System.Drawing.Size(296, 17);
+            this.cpuSwitch.TabIndex = 5;
+            this.cpuSwitch.Text = "x86 MC - 1.18.2 DID ONLY";
+            this.cpuSwitch.UseVisualStyleBackColor = true;
+            // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.tableLayoutPanel1);
             this.ForeColor = System.Drawing.Color.Silver;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "MainForm";
             this.Opacity = 0.95D;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -160,6 +177,7 @@
         private System.Windows.Forms.Label creatorLabel;
         private System.Windows.Forms.Label whatThisDoesLabel;
         private System.Windows.Forms.Label whatIsThisLabel;
+        private System.Windows.Forms.CheckBox cpuSwitch;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using FadedToolWFA.Utils;
 
@@ -23,51 +17,68 @@ namespace FadedToolWFA
         string mcidGameVersPtr = "";
         struct UniquePtr
         {
-            public UniquePtr(int index, string didPtr, string mcidPtr)
+            public UniquePtr(int index, string didPtr, string mcidPtr, string x86DidPtr)
             {
                 this.index = index;
                 this.didPtr = didPtr;
                 this.mcidPtr = mcidPtr;
+                this.x86DidPtr = x86DidPtr;
             }
             public int index;
             public string didPtr;
             public string mcidPtr;
+            public string x86DidPtr;
         }
         //Yaami laami <3
-        /*
+/*
          Format:
-        SelectedIndex, DidPointer, McidPointer
-        */
+        SelectedIndex, DidPointer, McidPointer, x86DidPointer
+*/
         List<UniquePtr> Versions = new List<UniquePtr> {
-    new UniquePtr(-1, "0421B218,0,20,0", "04249B70,0,30,18,50,0"),//If user hasn't selected a game version, just just latets and copy 0
-    new UniquePtr(0, "0421B218,0,20,0", "04249B70,0,30,18,50,0"),
-    new UniquePtr(1, "04215D08,0,20,0", "04244650,0,30,20,60,0"),
-    new UniquePtr(2, "041F4318,0,20,0", "042175D0,0,30,20,60,0"),
-    new UniquePtr(3, "041F32E8,0,20,0", ""),
-    new UniquePtr(4, "040776C8,3F8,218,50,2F0,0", ""),
-    new UniquePtr(5, "03F54B70,D8,10,D0,50,2F0,0", ""),
-    new UniquePtr(6, "03FE4618,0,4D0,280,30,40,2E0,0", ""),
-    new UniquePtr(7, "03CCB7A8,10,4D0,8,260,20,2E0,0", ""),
-    new UniquePtr(8, "037C7170,D0,48,218,50,2F0,0", ""),
-    new UniquePtr(9, "03A17598,10,80,0,8,290,0", ""),
-    new UniquePtr(10, "03582140,D0,48,80,0,8,290,0", ""),
-    new UniquePtr(11, "0368EE98,80,0,8,290,0", ""),
+    new UniquePtr(-1, "0458F1A8,0,20,0", "045CC590,0,30,20,60,0", ""),//If user hasn't selected a game version, just use latest which should be below
+
+    new UniquePtr(13, "0458F1A8,0,20,0", "045CC590,0,30,20,60,0", ""),//1.18.12
+    new UniquePtr(12, "0458F1C8,0,20,0", "045CC5F0,0,30,20,60,0", ""),//1.18.10
+    new UniquePtr(11, "0421B218,0,20,0", "04249B70,0,30,18,50,0", "03A87158,0,10,0"),
+    new UniquePtr(10, "04215D08,0,20,0", "04244650,0,30,20,60,0", ""),
+    new UniquePtr(9, "041F4318,0,20,0", "042175D0,0,30,20,60,0", ""),
+    new UniquePtr(8, "041F32E8,0,20,0", "", ""),
+    new UniquePtr(7, "040776C8,3F8,218,50,2F0,0", "", ""),
+    new UniquePtr(6, "03F54B70,D8,10,D0,50,2F0,0", "", ""),
+    new UniquePtr(5, "03FE4618,0,4D0,280,30,40,2E0,0", "", ""),
+    new UniquePtr(4, "03CCB7A8,10,4D0,8,260,20,2E0,0", "", ""),
+    new UniquePtr(3, "037C7170,D0,48,218,50,2F0,0", "", ""),
+    new UniquePtr(2, "03A17598,10,80,0,8,290,0", "", ""),
+    new UniquePtr(1, "03582140,D0,48,80,0,8,290,0", "", ""),
+    new UniquePtr(0, "0368EE98,80,0,8,290,0", "", ""),//1.16.40
 };
 
         private void versionSelectList_SelectedIndexChanged(object sender, EventArgs e)
         {
-            foreach (UniquePtr version in Versions)
-                if (versionSelectList.SelectedIndex == version.index)
-                {
-                    didGameVersPtr = version.didPtr;
-                    mcidGameVersPtr = version.mcidPtr;
-                }
+            if (!cpuSwitch.Checked)
+            {
+                foreach (UniquePtr version in Versions)
+                    if (versionSelectList.SelectedIndex == version.index)
+                    {
+                        didGameVersPtr = version.didPtr;
+                        mcidGameVersPtr = version.mcidPtr;
+                    }
+            } else
+            {
+                foreach (UniquePtr version in Versions)
+                    if (versionSelectList.SelectedIndex == version.index)
+                    {
+                        didGameVersPtr = version.x86DidPtr;
+                    }
+            }
         }
 
         private void spoofButton_Click(object sender, EventArgs e)
         {
-            Util.ChangeMCIDRandom(mcidGameVersPtr);
+            if (!cpuSwitch.Checked)
+                Util.ChangeMCIDRandom(mcidGameVersPtr);
             Util.ChangeIdRandom(didGameVersPtr);
+            MessageBox.Show("Spoofed");
         }
     }
 }
